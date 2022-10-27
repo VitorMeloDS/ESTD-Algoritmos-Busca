6378ad91c3b22803fcc47215e382814951599dd9
Vinícius Gonçalves <103079267+gonssalves@users.noreply.github.com>
2022-10-27 14:44:53 -0300

    Create busca_hibrida_time.py

---
+++ busca_hibrida_time.py
@@ -0,0 +1,43 @@
+import timeit
+import random
+
+minimo = 0
+maximo = 10000000
+
+qtd_de_vezes_repete_teste = 25
+
+def valor_aleatorio_a_procurar():
+  return random.randint(minimo,maximo*1.25)
+
+def busca_hibrida(uma_lista, item_pesquisado):
+    encontrou = False; tamanho_lista = len(uma_lista)
+    if tamanho_lista == 0: return 'Lista vazia'
+    elif tamanho_lista < 128:
+      for i in uma_lista:
+        if i == item_pesquisado: encontrou = True; break
+        elif i > item_pesquisado: encontrou = False; break
+  
+      return encontrou
+
+    else:
+      if len(uma_lista) == 0: return False
+      else:
+        meio = len(uma_lista)//2
+        if uma_lista[meio] == item_pesquisado: return True
+        else:
+          if item_pesquisado < uma_lista[meio]: return busca_hibrida(uma_lista[:meio],item_pesquisado)
+          else: return busca_hibrida(uma_lista[meio+1:],item_pesquisado)
+
+lista = list(range(maximo))
+
+def funcao_realiza_teste():
+
+  valor = valor_aleatorio_a_procurar() 
+  resultado = busca_hibrida(lista, valor)  
+  print(f'procurando o valor {valor} na lista de tamanho {len(lista)} resultado -> {resultado}')
+
+tempo = timeit.timeit( stmt=funcao_realiza_teste, number=qtd_de_vezes_repete_teste)
+
+print(f'teste repetido {qtd_de_vezes_repete_teste} vezes')
+print(f'tempo de duração do teste: {tempo}')
+print(f'tempo médio de duração do teste: {tempo/qtd_de_vezes_repete_teste}')

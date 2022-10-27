4110f1eabc974118584d7aa9ee369fefd1f7b45f
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-10-26 23:52:36 -0300

    feat: add vaidação para busca de números negativos

--- busca_hibrida.py
+++ busca_hibrida.py
@@ -4,39 +4,47 @@ from time import sleep
 
 if __name__ == '__main__':
 
-  clear = lambda: system('cls' if name == 'nt' else 'clear')
-  clear()
+  clearTerm = lambda: system('cls' if name == 'nt' else 'clear')
+  clearTerm()
 
   def apresentacao() -> bool:
     inicializador = input('\nPara iniciar o processo digite "start", para sair digite "exit": ').lower().strip()
-    print('\nVerificar se um número está em uma lista ordenada.')
     if inicializador == 'start':
+      print('\nVerificar se um número está em uma lista ordenada.')
       return True
     elif inicializador == 'exit':
       return False
     else:
-      print('\nComando não reconhecido, digite uns dos comando expecificados!'); sleep(2); clear(); apresentacao()
+      print('\nComando não reconhecido, digite uns dos comando expecificados!'); sleep(2); clearTerm(); apresentacao()
       return True
 
   def busca_hibrida(uma_lista, item_pesquisado):
+    if item_pesquisado == 0: return True
+
     encontrou = False; tamanho_lista = len(uma_lista)
+
+    if (item_pesquisado < 0):
+      novaLitsa = uma_lista
+      uma_lista = []
+      uma_lista = [i for i in range(len(novaLitsa))]
+      item_pesquisado = item_pesquisado * -1
     
     if tamanho_lista == 0: return 'Lista vazia'
     elif tamanho_lista < 128:
-        for i in uma_lista:
-            if i == item_pesquisado: encontrou = True; break
-            elif i > item_pesquisado: encontrou = False; break
-    
-        return encontrou
+      for i in uma_lista:
+        if i == item_pesquisado: encontrou = True; break
+        elif i > item_pesquisado: encontrou = False; break
+  
+      return encontrou
 
     else:
-        if len(uma_lista) == 0: return False
+      if len(uma_lista) == 0: return False
+      else:
+        meio = len(uma_lista)//2
+        if uma_lista[meio] == item_pesquisado: return True
         else:
-            meio = len(uma_lista)//2
-            if uma_lista[meio] == item_pesquisado: return True
-            else:
-                if item_pesquisado < uma_lista[meio]: return busca_hibrida(uma_lista[:meio],item_pesquisado)
-                else: return busca_hibrida(uma_lista[meio+1:],item_pesquisado)
+          if item_pesquisado < uma_lista[meio]: return busca_hibrida(uma_lista[:meio],item_pesquisado)
+          else: return busca_hibrida(uma_lista[meio+1:],item_pesquisado)
 
   def entradaDado():
     try:
@@ -44,22 +52,23 @@ if __name__ == '__main__':
         entrada = input('\nInfome o tamanho da lista e o elemento procurado respectivamente (separado por espaço): ').strip()
         if entrada == 'exit': _exit(0)
         else:
-            entrada = entrada.split()
-            if len(entrada) < 2 or len(entrada) > 2:
-                print('\nValor inválido')
-                sleep(2); clear(); entradaDado()
-            else:
-                qtd_lista = int(entrada[0])
-                item_procurado = int(entrada[1])
-                lista = [i for i in range(qtd_lista)]
-                if busca_hibrida(lista, item_procurado): print('Valor encontrado')
-                else: print('Valor não encontrado')
+          entrada = entrada.split()
+          if len(entrada) < 2 or len(entrada) > 2:
+            print('\nValor inválido')
+            sleep(2); clearTerm(); entradaDado()
+          else:
+            qtd_lista = int(entrada[0])
+            item_procurado = int(entrada[1])
+            if qtd_lista > 0: lista = [i for i in range(qtd_lista)]
+            else: lista = [-1 * i for i in range(-1 * qtd_lista)]
+            if busca_hibrida(lista, item_procurado): print(f'{item_procurado} encontrado')
+            else: print(f'{item_procurado} não encontrado')
                 
     except Exception as e:
       if e:
         print(e)
       print('\nComando não reconhecido, digite uns dos comando expecificados!')
-      sleep(2); clear(); entradaDado()
+      sleep(2); clearTerm(); entradaDado()
 
   def main():
     try:
